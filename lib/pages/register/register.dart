db2ee8c09d088ba559daa2adf308c4da73f3185b
Abinnnn <mr.adhityabayu@gmail.com>
2023-05-17 00:09:13 +0700

    Add-Bloc register fixed

--- lib/pages/register/register.dart
+++ lib/pages/register/register.dart
@@ -1,3 +1,5 @@
+import 'package:course_app/pages/register/bloc/register_event.dart';
+import 'package:course_app/pages/register/bloc/register_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -8,6 +10,7 @@ import '../sign_in/bloc/sign_in_blocs.dart';
 import '../sign_in/bloc/sign_in_events.dart';
 import '../sign_in/sign_in_controller.dart';
 import '../sign_in/widgets/sign_in_widget.dart';
+import 'bloc/register_bloc.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -19,80 +22,88 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: buildAppBar("Register"),
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 20.h,
-                ),
-                Center(
+    return BlocBuilder<RegisterBlocs, RegisterState>(builder: (context, state) {
+      return Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: buildAppBar("Register"),
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Center(
+                      child: reusableText(
+                          "Enter your details below & free to sign up")),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 36.h,
+                    ),
+                    padding: EdgeInsets.only(
+                      left: 25.w,
+                      right: 25.w,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //username section
+                        reusableText("Username"),
+                        buildTextField("Enter your username", "name", "user",
+                            (value) {
+                          context
+                              .read<RegisterBlocs>()
+                              .add(UserNameEvent(value));
+                        }),
+
+                        //email section
+                        reusableText("Email"),
+                        buildTextField(
+                            "Enter your email addres", "email", "user",
+                            (value) {
+                          context.read<RegisterBlocs>().add(EmailEvent(value));
+                        }),
+
+                        //password section
+                        reusableText("Password"),
+                        buildTextField(
+                            "Enter your email password", "password", "lock",
+                            (value) {
+                          context
+                              .read<RegisterBlocs>()
+                              .add(PasswordEvent(value));
+                        }),
+
+                        //re-connfirm password section
+                        reusableText("Re-enter Password"),
+                        buildTextField("Re-enter your password to confirm",
+                            "password", "lock", (value) {
+                          context
+                              .read<RegisterBlocs>()
+                              .add(RePasswordEvent(value));
+                        }),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 25.h,
+                    ),
                     child: reusableText(
-                        "Enter your details below & free to sign up")),
-                Container(
-                  margin: EdgeInsets.only(
-                    top: 36.h,
+                        "Enter your details below and free to sign up"),
                   ),
-                  padding: EdgeInsets.only(
-                    left: 25.w,
-                    right: 25.w,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //username section
-                      reusableText("Username"),
-                      buildTextField("Enter your username", "name", "user",
-                          (value) {
-                        // context.read<SignInBloc>().add(EmailEvent(value));
-                      }),
-
-                      //email section
-                      reusableText("Email"),
-                      buildTextField("Enter your email addres", "email", "user",
-                          (value) {
-                        // context.read<SignInBloc>().add(EmailEvent(value));
-                      }),
-
-                      //password section
-                      reusableText("Password"),
-                      buildTextField(
-                          "Enter your email password", "password", "lock",
-                          (value) {
-                        // context.read<SignInBloc>().add(PasswordEvent(value));
-                      }),
-
-                      //re-connfirm password section
-                      reusableText("Confirm your password"),
-                      buildTextField(
-                          "Enter your email password", "password", "lock",
-                          (value) {
-                        // context.read<SignInBloc>().add(PasswordEvent(value));
-                      }),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                    left: 25.h,
-                  ),
-                  child: reusableText(
-                      "Enter your details below and free to sign up"),
-                ),
-                buildLogInAdnRegButton("Sign Up", "login", () {
-                  Navigator.of(context).pushNamed("register");
-                }),
-              ],
+                  buildLogInAdnRegButton("Sign Up", "login", () {
+                    Navigator.of(context).pushNamed("register");
+                  }),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
