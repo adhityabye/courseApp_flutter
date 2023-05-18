a0a62aa2083809c37c683757329a5c8a7bdb30bc
Abinnnn <mr.adhityabayu@gmail.com>
2023-05-18 17:52:59 +0700

    Add-Routes logic

---
+++ lib/common/routes/pages.dart
@@ -0,0 +1,75 @@
+import 'package:course_app/pages/register/bloc/register_bloc.dart';
+import 'package:course_app/pages/register/register.dart';
+import 'package:course_app/pages/sign_in/bloc/sign_in_blocs.dart';
+import 'package:course_app/pages/sign_in/sign_in.dart';
+import 'package:course_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../pages/welcome/welcome.dart';
+import 'names.dart';
+
+//unify blocProvider and routes and page
+
+class AppPages {
+  static List<PageEntity> routes() {
+    return [
+      PageEntity(
+        route: AppRoutes.INITIAL,
+        page: const Welcome(),
+        bloc: BlocProvider(create: (_) => WelcomeBloc()),
+      ),
+      PageEntity(
+        route: AppRoutes.SIGN_IN,
+        page: const SignIn(),
+        bloc: BlocProvider(create: (_) => SignInBloc()),
+      ),
+      PageEntity(
+        route: AppRoutes.REGISTER,
+        page: const Register(),
+        bloc: BlocProvider(create: (_) => RegisterBlocs()),
+      ),
+      PageEntity(
+        route: AppRoutes.APPLICATION,
+        page: const Welcome(),
+        bloc: BlocProvider(create: (_) => WelcomeBloc()),
+      )
+    ];
+  }
+
+  static List<dynamic> allBlocProviders(BuildContext context) {
+    List<dynamic> blocProviders = <dynamic>[];
+    for (var bloc in routes()) {
+      blocProviders.add(bloc.bloc);
+    }
+    return blocProviders;
+  }
+
+  //a model that cover entire scree as we click on navigator object
+  static MaterialPageRoute GenerateRouteSettings(RouteSettings settings) {
+    if (settings.name != null) {
+      //check if route is available
+      var result = routes().where((element) => element.route == settings.name);
+      if (result.isNotEmpty) {
+        print("valid route name ${settings.name}}");
+        return MaterialPageRoute(
+            builder: (_) => result.first.page, settings: settings);
+      }
+    }
+    print("invalid route name ${settings.name}}");
+    return MaterialPageRoute(builder: (_) => SignIn(), settings: settings);
+  }
+}
+
+class PageEntity {
+  String route;
+  Widget page;
+  dynamic bloc;
+
+  PageEntity({
+    required this.route,
+    required this.page,
+    required this.bloc,
+  });
+}
