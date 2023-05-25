8eefd7c2d156bed27cf0d15feee514718f64d3b0
Abinnnn <mr.adhityabayu@gmail.com>
2023-05-25 15:59:00 +0700

    Remove-Button navigation refactor

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -1,10 +1,14 @@
-import 'package:course_app/pages/application/application_widget.dart';
+import 'package:course_app/pages/application/widgets/application_widget.dart';
+import 'package:course_app/pages/application/bloc/app_events.dart';
+import 'package:course_app/pages/application/bloc/app_states.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../common/values/colors.dart';
+import 'bloc/app_blocs.dart';
 
 class ApplicationPage extends StatefulWidget {
   const ApplicationPage({super.key});
@@ -17,125 +21,44 @@ class _ApplicationPageState extends State<ApplicationPage> {
   int _index = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          body: buildPage(_index),
-          bottomNavigationBar: Container(
-            width: 375.w,
-            height: 58.h,
-            decoration: BoxDecoration(
-                color: AppColors.primaryElement,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.h),
-                    topRight: Radius.circular(20.h)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.3),
-                    spreadRadius: 1,
-                    blurRadius: 1,
-                  ),
-                ]),
-            child: BottomNavigationBar(
-              currentIndex: _index,
-              onTap: (value) {
-                setState(() {
-                  _index = value;
-                });
-              },
-              elevation: 0,
-              type: BottomNavigationBarType.fixed,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              selectedItemColor: AppColors.primaryElement,
-              unselectedItemColor: AppColors.primaryFourthElementText,
-              items: [
-                BottomNavigationBarItem(
-                  label: "home",
-                  icon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset("assets/icons/home.png"),
-                  ),
-                  activeIcon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset(
-                      "assets/icons/home.png",
-                      color: AppColors.primaryElement,
+    return BlocBuilder<AppBlocs, AppState>(builder: (context, state) {
+      return Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: Scaffold(
+            body: buildPage(state.index),
+            bottomNavigationBar: Container(
+              width: 375.w,
+              height: 58.h,
+              decoration: BoxDecoration(
+                  color: AppColors.primaryElement,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.h),
+                      topRight: Radius.circular(20.h)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 1,
                     ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "search",
-                  icon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset("assets/icons/search2.png"),
-                  ),
-                  activeIcon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset(
-                      "assets/icons/search2.png",
-                      color: AppColors.primaryElement,
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "course",
-                  icon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset("assets/icons/play-circle1.png"),
-                  ),
-                  activeIcon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset(
-                      "assets/icons/play-circle1.png",
-                      color: AppColors.primaryElement,
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "chat",
-                  icon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset("assets/icons/message-circle.png"),
-                  ),
-                  activeIcon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset(
-                      "assets/icons/message-circle.png",
-                      color: AppColors.primaryElement,
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "profile",
-                  icon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset("assets/icons/person2.png"),
-                  ),
-                  activeIcon: SizedBox(
-                    width: 15.w,
-                    height: 15.h,
-                    child: Image.asset(
-                      "assets/icons/person2.png",
-                      color: AppColors.primaryElement,
-                    ),
-                  ),
-                ),
-              ],
+                  ]),
+              child: BottomNavigationBar(
+                currentIndex: state.index,
+                onTap: (value) {
+                  context.read<AppBlocs>().add(TriggerAppEvent(value));
+                },
+                elevation: 0,
+                type: BottomNavigationBarType.fixed,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                selectedItemColor: AppColors.primaryElement,
+                unselectedItemColor: AppColors.primaryFourthElementText,
+                items: bottonTabs,
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
