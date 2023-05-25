f3ea60c4a95e6b63534523524800c67815d84a31
Abinnnn <mr.adhityabayu@gmail.com>
2023-05-25 15:56:49 +0700

    Add-Homepage widgets

---
+++ lib/pages/home/widgets/home_page_widgets.dart
@@ -0,0 +1,245 @@
+import 'package:course_app/pages/home/bloc/home_page_bloc.dart';
+import 'package:course_app/pages/home/bloc/home_page_events.dart';
+import 'package:course_app/pages/home/bloc/home_page_states.dart';
+import 'package:course_app/pages/home/home_page.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../common/values/colors.dart';
+
+AppBar buildAppBar() {
+  return AppBar(
+    title: Container(
+      margin: EdgeInsets.only(left: 7.w, right: 7.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: 15.w,
+            height: 12.h,
+            child: Image.asset("assets/icons/menu.png"),
+          ),
+          GestureDetector(
+            child: Container(
+              width: 40.w,
+              height: 40.h,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/icons/person.png")),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+//reuseable title text
+Widget homePageText(String text,
+    {Color color = AppColors.primaryText, int top = 20}) {
+  return Container(
+    margin: EdgeInsets.only(top: 5.h),
+    child: Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: 24.sp,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
+
+Widget searchView() {
+  return Row(
+    children: [
+      Container(
+        width: 280.w,
+        height: 40.h,
+        decoration: BoxDecoration(
+          color: AppColors.primaryBackground,
+          borderRadius: BorderRadius.circular(15.h),
+          border: Border.all(
+            color: AppColors.primaryFourthElementText,
+          ),
+        ),
+        child: Row(
+          children: [
+            Container(
+                margin: EdgeInsets.only(left: 17.w),
+                width: 16.w,
+                height: 16.w,
+                child: Image.asset("assets/icons/search.png")),
+            Container(
+              width: 240.w,
+              height: 40.h,
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(5, 5, 0, 5),
+                    hintText: "search your course",
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    hintStyle: TextStyle(
+                      color: AppColors.primarySecondaryElementText,
+                    )),
+                style: TextStyle(
+                  color: AppColors.primaryText,
+                  fontFamily: "Avenir",
+                  fontWeight: FontWeight.normal,
+                  fontSize: 14.sp,
+                ),
+                autocorrect: false,
+                obscureText: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+      GestureDetector(
+        child: Container(
+          width: 40.w,
+          height: 40.h,
+          decoration: BoxDecoration(
+              color: AppColors.primaryElement,
+              borderRadius: BorderRadius.all(Radius.circular(13.w)),
+              border: Border.all(color: AppColors.primaryElement)),
+          child: Image.asset("assets/icons/options.png"),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget slidersView(BuildContext context, HomePageStates state) {
+  return Column(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: 20.h),
+        width: 325.w,
+        height: 160.w,
+        child: PageView(
+          onPageChanged: (value) {
+            print(value.toString());
+            context.read<HomePageBlocs>().add(HomePageDots(value));
+          },
+          children: [
+            _slidersContainer(path: "assets/icons/art.png"),
+            _slidersContainer(path: "assets/icons/Image(1).png"),
+            _slidersContainer(path: "assets/icons/Image(2).png"),
+          ],
+        ),
+      ),
+      Container(
+        child: DotsIndicator(
+          dotsCount: 3,
+          position: state.index.toDouble(),
+          decorator: DotsDecorator(
+              color: AppColors.primaryThirdElementText,
+              activeColor: AppColors.primaryElement,
+              size: const Size.square(5.0),
+              activeSize: const Size(17.0, 5.0),
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0))),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _slidersContainer({String path = "assets/icons/art.png"}) {
+  return Container(
+    margin: EdgeInsets.only(top: 20.h),
+    width: 325.w,
+    height: 160.w,
+    child: PageView(
+      children: [
+        Container(
+          width: 325.w,
+          height: 160.h,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20.h)),
+              image:
+                  DecorationImage(fit: BoxFit.fill, image: AssetImage(path))),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget menuView() {
+  return Column(
+    children: [
+      Container(
+        width: 325.w,
+        margin: EdgeInsets.only(top: 15.h),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            _reusableText("Choose your course"),
+            GestureDetector(
+              child: _reusableText("see all",
+                  color: AppColors.primaryThirdElementText, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 20.w),
+        child: Row(
+          children: [
+            _reusableMenuText("All"),
+            _reusableMenuText("Popular",
+                textColor: AppColors.primaryThirdElementText,
+                backGroundColor: Colors.white),
+            _reusableMenuText("Newest",
+                textColor: AppColors.primaryThirdElementText,
+                backGroundColor: Colors.white),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _reusableText(String text,
+    {Color color: AppColors.primaryText,
+    int fontSize = 16,
+    FontWeight fontWeight = FontWeight.bold}) {
+  return Container(
+    child: Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontWeight: fontWeight,
+        fontSize: fontSize.sp,
+      ),
+    ),
+  );
+}
+
+Widget _reusableMenuText(String menuText,
+    {Color textColor = AppColors.primaryElementText,
+    Color backGroundColor = AppColors.primaryElement}) {
+  return Container(
+    margin: EdgeInsets.only(right: 20.w),
+    decoration: BoxDecoration(
+        color: backGroundColor,
+        borderRadius: BorderRadius.circular(7.w),
+        border: Border.all(color: backGroundColor)),
+    padding: EdgeInsets.only(left: 15.w, right: 15.w, top: 5.h, bottom: 5.h),
+    child: _reusableText(menuText,
+        color: textColor, fontWeight: FontWeight.normal, fontSize: 12),
+  );
+}
