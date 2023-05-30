51ba41f506f0fb97c25e629930611f5718db544f
Abinnnn <mr.adhityabayu@gmail.com>
2023-05-30 11:08:10 +0700

    Add-SigIn blocs

---
+++ lib/pages/sign_in/bloc/sign_in_blocs.dart
@@ -0,0 +1,23 @@
+import 'package:course_app/pages/sign_in/bloc/sign_in_events.dart';
+import 'package:course_app/pages/sign_in/bloc/sign_in_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  SignInBloc() : super(const SignInState()) {
+    //state class
+    on<EmailEvent>(_emailEvent);
+    on<PasswordEvent>(_passwordEvent);
+  }
+
+  //event handler
+  void _emailEvent(EmailEvent event, Emitter<SignInState> emit) {
+    //print("my email is ${event.email}");
+    emit(state.copyWith(email: event.email));
+  }
+
+  //event handler
+  void _passwordEvent(PasswordEvent event, Emitter<SignInState> emit) {
+    //print("my password is ${event.password}");
+    emit(state.copyWith(password: event.password));
+  }
+}
